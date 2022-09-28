48a35495866040cb94bfdd862724d10d2c163583
Viet Tran <vtranseattle@gmail.com>
2022-09-28 15:36:58 -0700

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
-// import { Container, Navbar, NavbarBrand } from 'reactstrap';
-// import NucampLogo from './app/assets/img/logo.png';
-import CampsitesList from './features/campsites/CampsitesList.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <CampsitesList />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
 }
+
 export default App;
